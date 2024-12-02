11f7f7aacac0be5bd256ac22b8e3ea3806888f71
Nikita Larin <skydes2301@gmail.com>
2024-12-02 17:34:31 +0500

    Added bubble sort

--- 4_bubbleSort.js
+++ 4_bubbleSort.js
@@ -1,7 +1,16 @@
 const array = [ 1, 15, 2, 14, 3, 13, 4, 12, 5, 11, 6, 10, 7, 9, 8 ];
 
 function bubbleSort(arr) {
-
+	for ( let i = 0; i < arr.length; i++ ) {
+		for ( let j = 0; j < arr.length; j++ ) {
+			if ( arr[j] > arr[j + 1]) {
+				const tmp = arr[j]
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+	return arr
 }
 
 console.log(bubbleSort(array));
